504dd173461b8b6f088e92fca6cb87c63733434d
Rohit Choudhary <choudharyjji@hotmail.com>
2017-11-08 10:07:52 +0530

    android folder

---
+++ android/app/src/main/java/com/codewithrohit/MainActivity.java
@@ -0,0 +1,8 @@
+package com.codewithrohit;
+
+import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
+
+public class MainActivity extends SplashActivity {
+
+}
---
+++ android/app/src/main/java/com/codewithrohit/MainApplication.java
@@ -0,0 +1,44 @@
+package com.codewithrohit;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import com.reactnativenavigation.NavigationApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+
+  @Override
+  public boolean isDebug() {
+      // Make sure you are using BuildConfig from your own application
+      return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+      // Add additional packages you require here
+      // No need to add RnnPackage and MainReactPackage
+      return Arrays.<ReactPackage>asList(
+          new VectorIconsPackage()
+      );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+      return getPackages();
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+
+}
